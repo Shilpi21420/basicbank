991688d4b392c1b7ab5cceeb36421d2dea1e4216
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2021-01-15 16:44:45 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TSF Internship</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<ul id="left">
+			<li>SK &nbsp BANK</li>
+		</ul>
+
+		<ul id="right">
+			<li><a href="customer.php">CUSTOMER DETAILS</a></li>
+			<li><a href="view_transact.php">VIEW TRANSACTIONS</a></li>
+		</ul>
+	</header>
+	<section>
+	    <div class="sec_img">
+	    	<br><br>
+		    <div class="box">
+		    	<h1 style="text-align: center; font-size: 40px; font-style: oblique;">BASIC BANKING SYSTEM</h1><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		    	<h1 style="text-align: right; font-size: 25px">Created by,<br><br>SHILPI KUMARI</h1><br><br>
+		    	
+		    </div>
+		</div>
+	</section>
+
+
+</body>
+</html>
